5b4e4911f554babfaddc07230c12759007bf4ba3
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-05-23 19:59:56 +0200

    chore: implemented cypress

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -452,6 +452,14 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            // @juan required for transpiling the cypress code
+            {
+              test: /\.ts$/,
+              exclude: [/node_modules/],
+              use: [{
+                loader: 'ts-loader',
+              }],
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
