30d10efbdc917b450946d67d9dee1bd85a94b2d2
Jim Fung <jim@zimmermanzimmerman.nl>
2019-05-27 13:55:35 +0200

    refactor: storybook font hack

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import theme from 'app/theme';
-
+import 'index.css';
 const Providers: React.FunctionComponent = props => {
   return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
 };
