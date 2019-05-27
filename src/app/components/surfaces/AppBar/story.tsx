5b749c4b6e7648802fcc3b1ec98522beb795d59f
Jim Fung <jim@zimmermanzimmerman.nl>
2019-05-27 17:02:33 +0200

    feat: created AppBar and AppBarButton components
    closes [DS-29]

---
+++ src/app/components/surfaces/AppBar/story.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import AppBar from './index';
+import Providers from 'app/Providers';
+
+storiesOf('Surfaces', module).add('AppBar', () => (
+  <Providers>
+    <AppBar label="Large" size="large" />
+  </Providers>
+));
