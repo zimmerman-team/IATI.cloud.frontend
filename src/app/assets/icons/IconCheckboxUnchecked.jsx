597afff832b488abe92b1932474af38be17a0035
Jim Fung <jim@zimmermanzimmerman.nl>
2019-05-28 17:19:49 +0200

    feat(checkboxes): created unchecked icon

---
+++ src/app/assets/icons/IconCheckboxUnchecked.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IconCheckboxUnchecked = props => (
+  <svg viewBox="0 0 24 24" {...props}>
+    <rect x="3" y="3" width="18" height="18" rx="2" fill={props.color} />
+  </svg>
+);
+
+export default IconCheckboxUnchecked;
