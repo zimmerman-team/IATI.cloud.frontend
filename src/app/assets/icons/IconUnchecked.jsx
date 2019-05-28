b0d58d5cf7796ba2d3a0e0d974d984f88c1ce72b
Jim Fung <jim@zimmermanzimmerman.nl>
2019-05-28 13:11:54 +0200

    feat: added radiobutton icons

---
+++ src/app/assets/icons/IconUnchecked.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IconUnchecked = props => (
+  <svg viewBox="0 0 100 100" {...props}>
+    <circle fill={props.color} cx="50" cy="50" r="40" />
+  </svg>
+);
+
+export default IconUnchecked;
