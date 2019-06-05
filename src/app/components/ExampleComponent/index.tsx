ac4cd7dad217e1472494055bd9ba52227e4a5fe7
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-05 11:56:32 +0200

    refactor: cleaned up example component

--- src/app/components/ExampleComponent/index.tsx
+++ src/app/components/ExampleComponent/index.tsx
@@ -6,41 +6,12 @@ import IconChecked from 'app/assets/icons/IconChecked';
 import IconUnchecked from 'app/assets/icons/IconUnchecked';
 import theme from 'app/theme';
 type Props = {
-  size?: string;
   label?: string;
 };
 
-const Unnchecked = (
-  <SvgIcon>
-    <IconUnchecked color={theme.palette.grey.greyBase} />
-  </SvgIcon>
-);
-
-const Checked = (
-  <SvgIcon>
-    <IconChecked color={theme.palette.primary.main} />
-  </SvgIcon>
-);
-
-const BaseComponent = styled(props => (
-  <Radio
-    {...props}
-    disableRipple
-    icon={Unnchecked}
-    checkedIcon={Checked}
-    color="primary"
-  />
-))`
+const BaseComponent = styled(props => <Radio {...props} />)`
   && {
     & [class*='MuiIconButton-label'] {
-      height: 20px;
-      width: 20px;
-    }
-
-    & [class*='MuiSvgIcon-root'] {
-      display: flex;
-      justify-content: center;
-      align-items: center;
     }
   }
 `;
