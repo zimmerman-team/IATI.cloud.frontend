cd2ead42a2007c800470c9093c2e8abde9e5739f
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-06 18:10:56 +0200

    chore: removed props

--- src/app/components/surfaces/AppBar/story.tsx
+++ src/app/components/surfaces/AppBar/story.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import AppBar from './index';
+import AppBar from './';
 import Providers from 'app/Providers';
 
 storiesOf('Surfaces', module).add('AppBar', () => (
   <Providers>
-    <AppBar label="Large" size="large" />
+    <AppBar />
   </Providers>
 ));
