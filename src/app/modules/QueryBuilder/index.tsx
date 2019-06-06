568da72f6d98988a096767fb10ded889f42050b7
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-06 12:31:29 +0200

    feat: added base of querybuilder module

---
+++ src/app/modules/QueryBuilder/index.tsx
@@ -0,0 +1,11 @@
+/* base */
+import React from 'react';
+import styled from 'styled-components';
+
+const ComponentBase = styled.div``;
+
+const QueryBuilder: React.FunctionComponent = () => {
+  return <ComponentBase />;
+};
+
+export default QueryBuilder;
