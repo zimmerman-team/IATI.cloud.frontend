79a5d1d28debbfd7338cede943c4f51320c06109
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-06 18:12:14 +0200

    chore: created story

---
+++ src/app/modules/QueryBuilder/story.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import QueryBuilder from './';
+import AppBar from 'app/components/surfaces/AppBar';
+import Providers from 'app/Providers';
+
+storiesOf('Modules|Query Builder', module).add('Query Builder', () => (
+  <Providers>
+    <div>
+      <AppBar />
+      <QueryBuilder />
+    </div>
+  </Providers>
+));
