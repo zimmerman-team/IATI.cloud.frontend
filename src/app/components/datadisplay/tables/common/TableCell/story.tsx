1a6704dbadd67cd47661159ea5445456d11da4be
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-06 10:42:48 +0200

    feat: added table cell story

---
+++ src/app/components/datadisplay/tables/common/TableCell/story.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Component from './';
+import Providers from 'app/Providers';
+
+storiesOf('Data display|Table/common', module).add('Table Cell', () => (
+  <Providers>
+    <table>
+      <tbody>
+        <Component />
+      </tbody>
+    </table>
+  </Providers>
+));
