32a064a3bf270fd6be63b6f0db94c10611196c90
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-06-07 11:40:24 +0200

    fix: need to delete to make a dir name change of pageloader t PageLoader

--- src/app/modules/common/pageloader/comp.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageLoader from './index';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PageLoader />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
