aefe63c30720d6e9e7642a383aa1aa5f8294d2d2
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-11 16:50:27 +0200

    refactor: added navlink to outlined button

--- src/app/components/inputs/buttons/IconButtonOutlined/index.tsx
+++ src/app/components/inputs/buttons/IconButtonOutlined/index.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import ArrowForward from '@material-ui/icons/ArrowForward';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 type Props = {
   size?: string;
   label?: string;
 };
 
+const CustomLink = styled(props => <NavLink {...props} />)`
+  text-decoration: none;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
 const ZimmerButton = styled(props => <Button {...props} />)`
   && {
     justify-content: space-between;
-
+    color: white;
     width: 284px;
     height: ${props => {
       switch (props.size) {
@@ -33,9 +43,11 @@ const ZimmerButton = styled(props => <Button {...props} />)`
 
 const IconButtonOutlined: React.FC<Props> = props => {
   return (
-    <ZimmerButton size={props.size} variant="outlined" color="contrastText">
-      {props.label}
-      <ArrowForward fontSize="small" />
+    <ZimmerButton size={props.size} variant="outlined">
+      <CustomLink to="/querybuilder">
+        {props.label}
+        <ArrowForward fontSize="small" />
+      </CustomLink>
     </ZimmerButton>
   );
 };
