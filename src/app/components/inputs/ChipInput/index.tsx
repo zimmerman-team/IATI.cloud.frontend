fed2a19f192ebf103fc562405d9cabd1a6d22e9e
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-11 16:52:57 +0200

    refactor: changed file extension from jsx to tsx

---
+++ src/app/components/inputs/ChipInput/index.tsx
@@ -0,0 +1,66 @@
+import React, { ChangeEvent } from 'react';
+import ChipInput from 'material-ui-chip-input';
+import NoSsr from '@material-ui/core/NoSsr';
+import styled from 'styled-components';
+import Chip from 'app/components/datadisplay/Chip';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
+interface ChippieProps {
+  value?: any;
+}
+
+const Component = styled(props => <ChipInput {...props} />)`
+  && {
+    display: flex;
+    flex-direction: column;
+    background: #f0f3f7;
+    padding-left: 12px;
+    padding-top: 4px;
+    padding-bottom: 4px;
+    width: 600px;
+
+    & [class*='WAMuiChipInput-inputRoot'] {
+      display: flex;
+      align-items: center;
+      font-size: 14px;
+    }
+  }
+`;
+
+const Container: React.FC<ChippieProps> = props => {
+  return (
+    <NoSsr>
+      <Component
+        clickable="false"
+        disableUnderline
+        defaultValue={[
+          'foo',
+          'bar',
+          'lorem',
+          'ipsum',
+          'dolor',
+          'simet',
+          'foo',
+          'bar',
+          'lorem',
+          'ipsum',
+          'dolor',
+          'simet'
+        ]}
+        chipRenderer={(
+          { value, isFocused, isDisabled, handleClick, handleDelete },
+          key
+        ) => <Chip key={key} label={value} />}
+        InputLabelProps={{
+          shrink: false
+        }}
+        {...props}
+      />
+      <FormHelperText>
+        See OIPA documentation for full list of possible elements
+      </FormHelperText>
+    </NoSsr>
+  );
+};
+
+export default Container;
