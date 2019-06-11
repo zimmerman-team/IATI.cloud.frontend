502ce44c34e6ac278260152903544061cd141f5b
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-11 16:48:59 +0200

    refactor: removed borders on rows

--- src/app/components/datadisplay/tables/common/TableRow/index.tsx
+++ src/app/components/datadisplay/tables/common/TableRow/index.tsx
@@ -9,6 +9,12 @@ type Props = {
 const BaseComponent = styled(props => <Component {...props} />)`
   && {
     height: 50px;
+    & [class*='TableTreeContentBase-content'],
+    & [class*='MuiTableCell-root'] {
+      color: black;
+      border: initial;
+    }
+
     &:nth-child(even) {
       background-color: #f0f3f7;
     }
