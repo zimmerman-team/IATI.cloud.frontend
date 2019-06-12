f126e5b3c0b0c26b1fc18175a6ccbe0eecd1c9f5
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-12 14:26:30 +0200

    refactor: removed some redundant elements from story

--- src/app/modules/Landing/story.tsx
+++ src/app/modules/Landing/story.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Landing from 'app/modules/Landing';
-import AppBar from 'app/components/surfaces/AppBar';
 import Providers from 'app/Providers';
 
-storiesOf('Modules|Pages', module).add('Landing', () => (
+storiesOf('Modules|Landing', module).add('Landing', () => (
   <Providers>
-    <div>
-      <AppBar />
-      <Landing />
-    </div>
+    <Landing />
   </Providers>
 ));
