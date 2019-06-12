7cc193315c8270c90292120ed545bda3d017667e
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-12 14:20:38 +0200

    refactor: moved router to prodivers

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import theme from 'app/theme';
 import NoSsr from '@material-ui/core/NoSsr';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'index.css';
+
 const Providers: React.FunctionComponent = props => {
   return (
     <ThemeProvider theme={theme}>
       {/*<React.StrictMode>*/}
-      <NoSsr>{props.children}</NoSsr>
+      <NoSsr>
+        <Router>{props.children}</Router>
+      </NoSsr>
       {/*</React.StrictMode>*/}
     </ThemeProvider>
   );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Providers from './Providers';
-
 import Routes from './Routes';
 import AppBar from 'app/components/surfaces/AppBar';
 
 const App: React.FunctionComponent = () => {
   return (
     <Providers>
-      <Router>
-        <AppBar />
-        <Routes />
-      </Router>
+      <AppBar />
+      <Routes />
     </Providers>
   );
 };
