f8897c23355426245b3315edf35e29ad444ceb00
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-13 15:08:51 +0200

    refactor: cleaned up import

--- src/app/components/datadisplay/TooltipContent/story.tsx
+++ src/app/components/datadisplay/TooltipContent/story.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Component from './index';
+import Component from '.';
 import Providers from 'app/Providers';
 
 storiesOf('Data display|Tooltip', module).add('Tooltip Content', () => (
