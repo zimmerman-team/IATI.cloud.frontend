7e23aff6aa9ced1d0ba26f3a402f3af952f5289b
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-06-17 16:00:04 +0200

    style: centered the progress circle component

--- src/app/components/feedback/Progress/index.tsx
+++ src/app/components/feedback/Progress/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import styled from 'styled-components';
+
+const Base = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 const Progress: React.FC = props => {
   return (
-    <CircularProgress {...props} size={70} thickness={3.6} color="primary" />
+    <Base>
+      <CircularProgress {...props} size={70} thickness={3.6} color="primary" />
+    </Base>
   );
 };
 
