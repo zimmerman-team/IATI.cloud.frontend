7550edbdcfe9f8b7c30d834388139e918d4815f1
Jim Fung <jim@zimmermanzimmerman.nl>
2019-06-18 14:22:43 +0200

    refactor: removed some unneeded properties

--- src/app/components/surfaces/Background/index.tsx
+++ src/app/components/surfaces/Background/index.tsx
@@ -1,22 +1,18 @@
 /* base */
 import React from 'react';
 import styled from 'styled-components';
-
-/* components*/
 import BackgroundImage from 'app/assets/images/DottedMap.svg';
 
-const ComponentBase = styled.div`
-  width: 100vw;
+const Base = styled.div`
   height: 100vh;
   background-image: url(${BackgroundImage}),
     linear-gradient(to top, #155366, #06dbe4);
   background-repeat: no-repeat;
   background-position: center right;
-  z-index: -1;
 `;
 
 const Background: React.FC = props => {
-  return <ComponentBase>{props.children}</ComponentBase>;
+  return <Base>{props.children}</Base>;
 };
 
 export default Background;
