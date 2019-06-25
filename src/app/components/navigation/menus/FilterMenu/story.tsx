d17a74867d58b7554602eedc73705c8f27f3843c
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-06-25 10:58:33 +0200

    feat: implementation of filter menu

--- src/app/components/navigation/menus/FilterMenu/story.tsx
+++ src/app/components/navigation/menus/FilterMenu/story.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Component from './index';
 import Providers from 'app/Providers';
+import Button from '@material-ui/core/Button';
+import { Box } from '@material-ui/core';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
 const data = [
   [
@@ -20,13 +23,54 @@ const data = [
     [
       'Budget identifier',
       'Budget identifier sector',
+      'Budget identifier sector (catagory)',
+      'Budget identifier',
+      'Budget identifier sector',
+      'Budget identifier sector (catagory)',
+      'Budget identifier',
+      'Budget identifier sector',
       'Budget identifier sector (catagory)'
     ]
   ]
 ];
+const MyComponent: React.FC = () => {
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const [open, setOpen] = React.useState(false);
+
+  function handleToggle() {
+    setOpen(prevOpen => !prevOpen);
+  }
+
+  function handleClose(event: React.MouseEvent<EventTarget>) {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  }
+
+  return (
+    <Box>
+      <Button
+        ref={React.useRef<HTMLButtonElement>(null)}
+        aria-controls="menu-list-grow"
+        aria-haspopup="true"
+        onClick={() => handleToggle()}
+      >
+        Button look into source of this story for implementation example
+      </Button>
+      <ClickAwayListener onClickAway={handleClose}>
+        <Component data={data} anchorRef={anchorRef} open={open} />
+      </ClickAwayListener>
+    </Box>
+  );
+};
 
 storiesOf('Navigation|Menus/', module).add('FilterMenu', () => (
   <Providers>
-    <Component data={data} />
+    <MyComponent />
   </Providers>
 ));
