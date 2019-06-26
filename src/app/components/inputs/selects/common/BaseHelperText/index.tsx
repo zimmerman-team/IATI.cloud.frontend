03e21749c61f556e17742c022e27f846b39a4db1
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-06-26 10:46:18 +0200

    feat: implemented reusable component helperText

---
+++ src/app/components/inputs/selects/common/BaseHelperText/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from '@material-ui/core';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
+type Props = {
+  helperText?: string;
+  helperTextAfter?: string;
+  url?: string;
+  linkText?: string;
+};
+
+const BaseComponent = styled(props => <FormHelperText {...props} />)`
+  margin-top: 4px !important;
+  & [class*='MuiLink-underlineHover']:hover {
+    text-decoration: none;
+  }
+`;
+
+const BaseHelperText: React.FC<Props> = props => {
+  return (
+    <BaseComponent>
+      {props.helperText}
+      <Link href={props.url}>{props.linkText}</Link>
+      {props.helperTextAfter}
+    </BaseComponent>
+  );
+};
+
+export default BaseHelperText;
