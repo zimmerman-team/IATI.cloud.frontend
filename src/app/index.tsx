47797fcc8fffb010dc70204cf1c19812e7a99fbf
Jim Fung <jim@zimmermanzimmerman.nl>
2019-07-08 16:54:28 +0200

    refactor: simplified the index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,30 +4,21 @@ import Providers from 'app/Providers';
 import Routes from 'app/Routes';
 import AppBar from 'app/components/surfaces/AppBar';
 import SnackBar from 'app/components/feedback/SnackBar';
-import { useActions, useStore } from 'app/store';
 
 type AppProps = {
   openSnackbar?: boolean;
 };
 
-const App: React.FunctionComponent<AppProps> = props => {
+function App(props: AppProps) {
   return (
     <Providers>
       <Router>
         <AppBar data-cy="appbar" />
         <Routes />
-        <SnackBar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left'
-          }}
-          variant="info"
-          message="New content is available and will be used when all tabs of this page are closed"
-          open={props.openSnackbar}
-        />
+        <SnackBar open={props.openSnackbar} />
       </Router>
     </Providers>
   );
-};
+}
 
 export default App;
