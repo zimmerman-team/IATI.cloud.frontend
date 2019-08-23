9d81401b87f4d4af0ecc77ac2a58c92d1c826713
Jim Fung <jim@zimmermanzimmerman.nl>
2019-08-23 13:38:19 +0200

    refactor: modified breakpoints

--- src/app/theme/index.js
+++ src/app/theme/index.js
@@ -1,5 +1,4 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-
 export const Spacing = {
   inputSideSpacing: '15px',
 };
@@ -74,7 +73,7 @@ const Breakpoints = {
   xs: 0,
   sm: 600,
   md: 960,
-  lg: 1280,
+  lg: 1024,
   xl: 1920,
 };
 
@@ -208,4 +207,14 @@ export default createMuiTheme({
   mixins: Mixins,
   shape: Shape,
   zIndex: zIndex,
+  breakpoints:{
+    values: {
+      xs: 0,
+      sm: 450,
+      md: 600,
+      lg: 1088,
+      xl: 1280,
+      xxl: 1920,
+    }
+  }
 });
