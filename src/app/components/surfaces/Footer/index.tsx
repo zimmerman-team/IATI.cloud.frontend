073c773e3cae09b51167efdb3bc2a74382cc6bbd
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-08-26 10:38:18 +0200

    feat: implemented footer component

--- src/app/components/surfaces/Footer/index.tsx
+++ src/app/components/surfaces/Footer/index.tsx
@@ -1,45 +1,40 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import styled from 'styled-components';
-import MuiBox from '@material-ui/core/Box';
-// import Colors from 'app/theme/color';
-import MuiLink from '@material-ui/core/Link';
-import { Grid, Typography, Container, Box } from '@material-ui/core';
+import {
+  Grid,
+  Typography as MuiTypography,
+  Container,
+  Box,
+} from '@material-ui/core';
 import { Mail, Phone, Star } from '@material-ui/icons';
 import { IATILogoWhite } from 'app/components/svgs/IATILogo';
 import { ZZLogo } from 'app/components/svgs/ZZLogo';
+import styled from 'styled-components';
 
-// import { DILogo } from 'app/components/svgs/DevelopmentInitiatives';
-// import { MONLogo } from 'app/components/svgs/MinistryOfNetherlands';
+//TODO: When Text Catalog is implemented in Zeplin, refactor to use the right Typography variants
+const Typography1 = styled(props => (
+  <MuiTypography {...props} color="textSecondary" />
+))`
+  && {
+    font-family: Chalet-LondonNineteenSixty;
+    font-size: 16px;
+    line-height: 2.25;
+  }
+`;
 
-// const Box = styled(props => <MuiBox {...props} />)`
-//   && {
-//     //background-color: ${Colors.branddark};
-//     padding: 32px 54px;
-//     position: absolute;
-//     left: 0;
-//     bottom: 0;
-//     width: calc(100% - 108px);
-//   }
-// `;
-//
-// const Link = styled(props => <MuiLink {...props} />)`
-//   && {
-//     font-family: Inter;
-//     font-size: 1rem;
-//     font-weight: 500;
-//     line-height: 1.5;
-//     letter-spacing: 0.5px;
-//     //color: ${Colors.whiteOrFontlightbase};
-//     :hover {
-//       cursor: pointer;
-//     }
-//   }
-// `;
+const Typography2 = styled(props => (
+  <MuiTypography {...props} color="textSecondary" />
+))`
+  && {
+    font-family: Chalet-LondonNineteenSixty;
+    font-size: 14px;
+    line-height: 1.43;
+  }
+`;
 
 export const Footer = () => {
   return (
-    <Container>
+    <Container maxWidth={false}>
       <Box bgcolor="#155366" height="200px" width="100%">
         {/*LEFT ITEM*/}
         <Grid
@@ -47,6 +42,7 @@ export const Footer = () => {
           direction="row"
           justify="space-around"
           alignItems="center"
+          style={{ height: '200px' }}
         >
           <Grid item>
             <IATILogoWhite />
@@ -55,38 +51,54 @@ export const Footer = () => {
           {/*MIDDLE ITEM*/}
           <Grid item>
             <Grid container direction="column" justify="space-between">
-              <Grid item>
-                <Phone style={{ marginLeft: '16px' }} />
-                <Typography variant="caption">+31 (0)00 000 00 00</Typography>
+              <Grid container item alignItems="center">
+                <Phone style={{ marginRight: '16px', fill: 'white' }} />
+                <Typography1 variant="caption">+31 (0)00 000 00 00</Typography1>
               </Grid>
-
-              <Grid item>
-                <Mail style={{ marginLeft: '16px' }} />
-                <Typography variant="caption">
-                  info@@iatistandard.org
-                </Typography>
+              <Grid container item alignItems="center">
+                <Mail style={{ marginRight: '16px', fill: 'white' }} />
+                <Typography1 variant="caption">
+                  info@iatistandard.org
+                </Typography1>
               </Grid>
-
-              <Grid item>
-                <Star style={{ marginLeft: '16px' }} />
-                <Typography variant="caption">@iati</Typography>
+              <Grid container item alignItems="center">
+                <Star style={{ marginRight: '16px', fill: 'white' }} />
+                <Typography1 variant="caption">@iati</Typography1>
               </Grid>
             </Grid>
           </Grid>
 
           {/*RIGHT ITEM*/}
           <Grid item>
-            <Grid container direction="column" justify="space-between">
-              <Grid item alignContent="center" alignItems="center">
-                <Typography variant="caption">powered by OIPA from </Typography>
-                <ZZLogo />
+            <Grid
+              container
+              direction="column"
+              justify="space-between"
+              alignItems="center"
+            >
+              <Grid item>
+                <Grid container item alignItems="center">
+                  <Typography2
+                    variant="caption"
+                    style={{ marginRight: '16px' }}
+                  >
+                    powered by OIPA from
+                  </Typography2>
+                  <ZZLogo />
+                </Grid>
               </Grid>
 
-              <Grid item>
-                <Typography variant="caption">
+              {/*This one is confusing, couldn't get the type to align center.*/}
+              <Grid
+                container
+                // item
+                style={{ marginTop: '32px', width: '350px' }}
+                wrap="wrap"
+              >
+                <Typography2 variant="caption" align="center">
                   Code licensed under the GNU AGPL. Documentation licensed under
-                  CC BY 3.0.{' '}
-                </Typography>
+                  CC BY 3.0.
+                </Typography2>
               </Grid>
             </Grid>
           </Grid>
