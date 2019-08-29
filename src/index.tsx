2e23da576f8feb154444b2e3e89c841bf5a1f637
Jim Fung <jim@zimmermanzimmerman.nl>
2019-08-29 08:44:28 +0200

    chore: disabled serviceworker to prevent hard-cache in development

--- src/index.tsx
+++ src/index.tsx
@@ -20,9 +20,10 @@ if (rootElement != null) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
-if (process.env.REACT_APP_NODE_ENV === 'development') {
+/*if (process.env.REACT_APP_NODE_ENV === 'development') {
   serviceWorker.unregister();
 } else {
   serviceWorker.register();
-}
+}*/
