f06f9af58054ef47c8920b1a89b9c38e37db2648
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-09-02 18:48:11 +0300

    feat: flow of adding extra filter category

--- src/app/modules/QueryBuilder/fragments/FilterFragment/index.tsx
+++ src/app/modules/QueryBuilder/fragments/FilterFragment/index.tsx
@@ -2,58 +2,80 @@
 import React from 'react';
 /* third-party */
 import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
-import Add from '@material-ui/core/SvgIcon/SvgIcon';
-/* project component */
-import MultiSelectChip from 'app/components/inputs/selects/MultiSelectChip';
-import FilterMenu from 'app/components/navigation/menus/FilterMenu';
-import SimpleSelect from 'app/components/inputs/selects/SimpleSelect';
-import DateField from 'app/components/inputs/textinputs/DateInputField';
-import IconButton from 'app/components/inputs/buttons/IconButton';
-/* common */
-import { ModuleFragment } from 'app/modules/QueryBuilder/fragments/common';
-/* config & mock */
-import { fragmentConfig } from './model';
-import FilterData from 'app/components/navigation/menus/FilterMenu/mock';
 import { TextField } from '@material-ui/core';
+/* project component */
+import { AddFilterModule } from 'app/components/utils/Popover';
+import { ModuleStore } from 'app/modules/QueryBuilder/state/store';
+import { ConnectedSelect } from 'app/components/inputs/selects/ConnectedSelect';
+/* utils */
+import find from 'lodash/find';
+import { additionalFiltersPopData } from './consts';
 
 export const FilterFragment = () => {
-  const anchorRef = React.useRef<HTMLButtonElement>(null);
-  const [openFilter, setOpenFilter] = React.useState(false);
-  function handleToggle() {
-    setOpenFilter(prevOpen => !prevOpen);
-  }
+  const store = ModuleStore.useStore();
+  const setAdditionFilters = e => {
+    store.set('additionalFilters')(e);
+  };
+
+  const addedFilterOptions = store.get('additionalFilters');
 
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} sm={12} md={12}>
-        <TextField />
+        <TextField
+          fullWidth
+          autoFocus
+          margin="normal"
+          variant="outlined"
+          placeholder="Text search"
+          label="Data title,activity or descriptions"
+          helperText="Have minium 1-2 other filters selected to avoid searching the entire database"
+        />
+      </Grid>
+
+      <Grid item xs={12} sm={12} md={6}>
+        <ConnectedSelect
+          label="Transaction Provider Org"
+          options={[]}
+          getOptionValue={option => option.code}
+          getOptionLabel={option => `${option.code}: ${option.name}`}
+        />
+      </Grid>
+
+      <Grid item xs={12} sm={12} md={6}>
+        <ConnectedSelect
+          label="Transaction Receiver Org"
+          options={[]}
+          getOptionValue={option => option.code}
+          getOptionLabel={option => `${option.code}: ${option.name}`}
+        />
       </Grid>
 
       <Grid item xs={12} sm={12} md={12}>
-        <Divider />
+        <ConnectedSelect
+          label="Participating Organisation"
+          options={[]}
+          getOptionValue={option => option.code}
+          getOptionLabel={option => `${option.code}: ${option.name}`}
+        />
       </Grid>
 
+      {addedFilterOptions.map(filter => {
+        const allAddFilters = [
+          ...additionalFiltersPopData[0][1],
+          ...additionalFiltersPopData[1][1],
+        ];
+        return find(allAddFilters, { label: filter }).component({
+          store,
+        });
+      })}
+
       <Grid item xs={12} sm={12} md={4}>
-        <SimpleSelect label="Must have activity periode" data={['Yes', 'No']} />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <DateField />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <DateField />
-      </Grid>
-      <Grid item xs={12} sm={12}>
-        <Divider />
-      </Grid>
-      <Grid item xs={12} sm={12} md={4}>
-        <IconButton
-          label="Add Filter"
-          icon={<Add />}
-          onClick={() => handleToggle()}
-          anchorref={anchorRef}
+        <AddFilterModule
+          data={additionalFiltersPopData}
+          onCheckChange={setAdditionFilters}
+          addedFilterOptions={addedFilterOptions}
         />
-        <FilterMenu data={FilterData} anchorRef={anchorRef} open={openFilter} />
       </Grid>
     </Grid>
   );
