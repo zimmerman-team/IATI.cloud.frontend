844653f50bf1dc7e6dba5fb305a6b319ae05c59a
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-09-02 18:41:36 +0300

    refactor: extra filter options popover to work with real data

--- src/app/components/utils/Popover/index.tsx
+++ src/app/components/utils/Popover/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import IconButton from 'app/components/inputs/buttons/IconButton';
 
 import ListControls from 'app/components/datadisplay/Lists/ListControls';
-import data from 'app/components/datadisplay/Lists/ListControls/mock';
 import { Add } from '@material-ui/icons';
 import { Popover } from '@material-ui/core';
 
-export const AddFilterModule = (props) => {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+export const AddFilterModule = props => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
 
   function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     setAnchorEl(event.currentTarget);
@@ -22,11 +23,7 @@ export const AddFilterModule = (props) => {
 
   return (
     <>
-    <IconButton
-      icon={<Add/>}
-      label="Add Filter"
-      onClick={handleClick}>
-    </IconButton>
+      <IconButton icon={<Add />} label="Add Filter" onClick={handleClick} />
       <Popover
         id={id}
         open={open}
@@ -41,7 +38,11 @@ export const AddFilterModule = (props) => {
           horizontal: 'left',
         }}
       >
-        <ListControls data={data}/>
+        <ListControls
+          data={props.data}
+          onCheckChange={props.onCheckChange}
+          addedFilterOptions={props.addedFilterOptions}
+        />
       </Popover>
     </>
   );
