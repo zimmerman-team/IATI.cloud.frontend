6edf93f83901881017f040c42b616de8108c9e62
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-09-03 11:25:59 +0300

    feat: add all step 1 filters into layout

--- src/app/modules/QueryBuilder/steps/organisation/index.tsx
+++ src/app/modules/QueryBuilder/steps/organisation/index.tsx
@@ -1,16 +1,22 @@
-import { Grid } from '@material-ui/core';
-import { ConnectedSelect } from 'app/components/inputs/selects/ConnectedSelect';
-import { Skeletor } from 'app/components/utils/Skeletor';
-import { ModuleStore } from 'app/modules/QueryBuilder/state/store';
-import { useStoreState } from 'app/state/store';
+/* eslint-disable react/jsx-props-no-spreading */
+/* core */
 import React from 'react';
+/* third-party */
+import { Grid } from '@material-ui/core';
+/* project component */
+import SimpleSelect from 'app/components/inputs/selects/SimpleSelect';
+import DateField from 'app/components/inputs/textinputs/DateInputField';
+import { ConnectedSelect } from 'app/components/inputs/selects/ConnectedSelect';
 import { fragmentConfig } from 'app/modules/QueryBuilder/steps/organisation/model';
+/* utils */
 import get from 'lodash/get';
+import { useStoreState } from 'app/state/store';
+import { ModuleStore } from 'app/modules/QueryBuilder/state/store';
 
 export const OrganisationFragment = () => {
   const store = ModuleStore.useStore();
 
-  /* getting the fetched datat from the app store */
+  /* getting the fetched data from the app store */
   const fetchedOrganisationTypes = useStoreState(
     state => state.organisationTypes.data
   );
@@ -25,20 +31,6 @@ export const OrganisationFragment = () => {
     <Grid container spacing={4}>
       {/** reporting organisation*/}
       <Grid item xs={12} md={12}>
-        {/** <Skeletor />*/}
-
-        <ConnectedSelect
-          {...fragmentConfig.organisationTypes}
-          options={fetchedOrganisationTypes}
-          value={store.get('organisationTypes')}
-          onChange={e => store.set('organisationTypes')(e)}
-          placeholder={`All (${get(fetchedOrganisationTypes, 'length', 0)})`}
-        />
-      </Grid>
-      {/** type of organisation */}
-      <Grid item xs={12} md={8}>
-        {/** <Skeletor />*/}
-
         <ConnectedSelect
           {...fragmentConfig.organisations}
           value={store.get('organisations')}
@@ -47,45 +39,69 @@ export const OrganisationFragment = () => {
           placeholder={`All (${get(fetchedsectorOrganisations, 'length', 0)})`}
         />
       </Grid>
+      {/** type of organisation */}
+      <Grid item xs={12} md={8}>
+        <ConnectedSelect
+          {...fragmentConfig.organisationTypes}
+          options={fetchedOrganisationTypes}
+          value={store.get('organisationTypes')}
+          onChange={e => store.set('organisationTypes')(e)}
+          placeholder={`All (${get(fetchedOrganisationTypes, 'length', 0)})`}
+        />
+      </Grid>
       {/** secondary publisher */}
       <Grid item xs={12} md={4}>
-        <Skeletor />
+        <ConnectedSelect
+          {...fragmentConfig.secondaryPublisher}
+          options={[]}
+          value={[]}
+        />
       </Grid>
       {/** sector */}
       <Grid item xs={12} md={6}>
-        {/** <Skeletor />*/}
-
         <ConnectedSelect
           {...fragmentConfig.organisationSector}
+          options={[]}
+          value={[]}
+        />
+      </Grid>
+      {/** sector category */}
+      <Grid item xs={12} md={6}>
+        <ConnectedSelect
+          {...fragmentConfig.sectorCategory}
           value={store.get('sectorCategories')}
           options={fetchedsectorCategories}
           onChange={e => store.set('sectorCategories')(e)}
           placeholder={`All (${get(fetchedsectorCategories, 'length', 0)})`}
         />
       </Grid>
-      {/** sector category */}
-      <Grid item xs={12} md={6}>
-        <Skeletor />
-      </Grid>
       {/** recipient country */}
       <Grid item xs={12} md={12}>
-        <Skeletor />
+        <ConnectedSelect
+          {...fragmentConfig.recipientCountry}
+          options={[]}
+          value={[]}
+        />
       </Grid>
       {/** recipient region */}
       <Grid item xs={12} md={12}>
-        <Skeletor />
+        <ConnectedSelect
+          {...fragmentConfig.recipientRegion}
+          options={[]}
+          value={[]}
+        />
       </Grid>
       {/** must have activity period */}
       <Grid item xs={12} md={4}>
-        <Skeletor />
+        <SimpleSelect label="Must have activity periode" data={['Yes', 'No']} />
       </Grid>
       {/** activity period start date */}
       <Grid item xs={12} md={4}>
-        <Skeletor />
+        <DateField />
       </Grid>
       {/** activity period end date */}
       <Grid item xs={12} md={4}>
-        <Skeletor />
+        <DateField />
       </Grid>
     </Grid>
   );
