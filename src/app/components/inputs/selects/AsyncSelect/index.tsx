3020a692e0d8b7b82cba777a6783708b28e8720d
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-09-09 19:18:49 +0300

    chore: remove special characters from search regex

--- src/app/components/inputs/selects/AsyncSelect/index.tsx
+++ src/app/components/inputs/selects/AsyncSelect/index.tsx
@@ -32,7 +32,7 @@ const customStyles = {
 
 async function loadOptions(search, loadedOptions, { page }, pivot) {
   const url = `https://test-datastore.iatistandard.org/dev/search/activity/select?q=${pivot}:*&facet=on&facet.pivot=${pivot}&rows=0&facet.sort=${pivot}&facet.limit=10&facet.offset=${page *
-    10}&facet.matches=^${search}.*$`;
+    10}&facet.matches=^${search.replace(/[^a-zA-Z ]/g, '')}.*$`;
   const response = await fetch(url);
   const responseJSON = await response.json();
 
