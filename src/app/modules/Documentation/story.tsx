fb3df92bc571b6c011643ee3a6c4c791d231394f
Jim Fung <jim@zimmermanzimmerman.nl>
2019-09-18 11:06:49 +0200

    feat: api documentation story

--- src/app/modules/Documentation/story.tsx
+++ src/app/modules/Documentation/story.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import {DocModule} from '.';
-import Providers from 'app/Providers';
 
-storiesOf('Modules|Query Builder', module).add('Query Builder', () => (
+import Providers from 'app/Providers';
+import { Container, Grid } from '@material-ui/core';
+import { DebugBox } from 'app/utils/layout';
+import { DocsideBar } from 'app/modules/Documentation/common/DocSideBar';
+import { DocDetailModule } from "app/modules/Documentation/common/DocDetail";
+import { DocExampleModule } from "app/modules/Documentation/common/DocExample";
+
+storiesOf('Modules|API Documentation', module).add('Detail total', () => (
   <Providers>
-    <DocModule />
+    <DebugBox>
+
+      <Container maxWidth="xxl">
+        <Grid container spacing={4}>
+          <Grid item md={3}>
+            <DocsideBar />
+          </Grid>
+          <Grid item md={5}>
+            <DocDetailModule />
+          </Grid>
+          <Grid item md={4}>
+            <DocExampleModule />
+          </Grid>
+        </Grid>
+      </Container>
+    </DebugBox>
   </Providers>
 ));
