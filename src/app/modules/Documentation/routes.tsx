c9128b03528bbea323c87d984aec1135159a1f03
Jim Fung <jim@zimmermanzimmerman.nl>
2019-09-27 16:57:00 +0200

    refactor: added url parameters to docdetail route

--- src/app/modules/Documentation/routes.tsx
+++ src/app/modules/Documentation/routes.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { OrganisationFragment } from 'app/modules/QueryBuilder/steps/organisation';
-import { AdditionalFiltersSubModule } from 'app/modules/QueryBuilder/steps/additional-filters';
-import { OutputFormatSubModule } from 'app/modules/QueryBuilder/steps/output-format';
-import { ResultsSubModule } from 'app/modules/QueryBuilder/steps/results';
 import { DocDetail } from 'app/modules/Documentation/common/DocDetail';
 import { DocLanding } from 'app/modules/Documentation/common/DocLanding';
+import { InView } from 'react-intersection-observer';
+import { Grid } from '@material-ui/core';
 
 export const DocumentationRoutes = () => {
   return (
-    <Switch>
-      <Route
-        exact
-        path="/documentation/introduction"
-        render={() => <DocLanding />}
-      />
-      <Route path="/documentation/:id" component={DocDetail} />
-    </Switch>
+    <div
+      css={`
+        position: sticky;
+        top: 187px;
+        overflow-y: scroll;
+        height: calc(100vh - 187px);
+        padding-left: 20px;
+        padding-right: 20px;
+      `}
+    >
+      <Switch>
+        <Route
+          exact
+          path="/documentation/introduction"
+          render={() => <DocLanding />}
+        />
+        <Route
+          path="/documentation/:_postman_id"
+          render={({ match }) => (
+            <DocDetail _postman_id={match.params._postman_id} />
+          )}
+        />
+      </Switch>
+    </div>
   );
 };
