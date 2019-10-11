40cbdf2a77c7b1d1cfc1d5ae4e12390d3f2f48e7
Jim Fung <jim@zimmermanzimmerman.nl>
2019-10-11 15:17:09 +0200

    test: serviceworker is throwing errors, disabled for now

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ if (rootElement != null) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 /*if (process.env.REACT_APP_NODE_ENV === 'development') {
   serviceWorker.unregister();
