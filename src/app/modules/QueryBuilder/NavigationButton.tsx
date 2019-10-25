f85947d7ebdf415bfd3e8cf19f90df30531eed0f
Jim Fung <jim@zimmermanzimmerman.nl>
2019-10-25 15:47:26 +0200

    feat: made qb step nav responsive

--- src/app/modules/QueryBuilder/NavigationButton.tsx
+++ src/app/modules/QueryBuilder/NavigationButton.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 import { NavButtonModel } from './layout';
 export const NavigationButton = (props: NavButtonModel) => {
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.up('md'));
+
   return (
     <NavLink
       to={props.path}
       activeStyle={{ borderBottom: '4px solid #03dbe4' }}
     >
       <Box
-        width="235px"
+        width={md ? '135px' : '235px'}
         height="30px"
         bgcolor="grey"
         display="flex"
