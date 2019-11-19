63ea5ad0773f0c2f5ce681c6e11607b6295bf84b
Lu Min Han <luminn@zimmermanzimmerman.nl>
2019-11-19 12:29:33 +0100

    fix: postman doc url changed

--- src/app/modules/Documentation/state/service.ts
+++ src/app/modules/Documentation/state/service.ts
@@ -2,7 +2,7 @@ import { Endpoint } from 'app/state/interfaces/Endpoint';
 
 export const getPostmanData: Endpoint = (params: RequestInit = {}) =>
   fetch(
-    'https://datastore.iati.cloud/backend/static/postman/postman_json.json',
+    'https://datastore.iati.cloud/static/postman/postman_json.json',
     {
       method: 'GET',
     }
