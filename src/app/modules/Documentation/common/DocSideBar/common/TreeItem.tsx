8442b87a99207796711b1df4bb698dccde19e391
Jim Fung <jim@zimmermanzimmerman.nl>
2020-06-16 14:30:53 +0200

    refactor: review

--- src/app/modules/Documentation/common/DocSideBar/common/TreeItem.tsx
+++ src/app/modules/Documentation/common/DocSideBar/common/TreeItem.tsx
@@ -1,26 +1,27 @@
 // @ts-nocheck
 //cc:api documentation module#; navigation - side bar
-import React from 'react';
-import { TreeItem } from '@material-ui/lab';
-import { HashLink } from 'react-router-hash-link';
-import styled from 'styled-components';
-import { SubLink3 } from './ui/SubLink3';
+import React from "react";
+import { TreeItem } from "@material-ui/lab";
+import { HashLink } from "react-router-hash-link";
+import styled from "styled-components";
+import { SubLink3 } from "./ui/SubLink3";
 
 type Props = {
   item: any;
   postmanId: string;
 };
 
-const Link = styled(props => (<HashLink {...props} />))`
+const Link = styled((props) => <HashLink {...props} />)`
   && {
-      text-decoration: none;
-      color: rgba(1,1,10,0.6);
+    text-decoration: none;
+    color: rgba(1, 1, 10, 0.6);
   }
 `;
 
-const Item = styled(props => (<TreeItem {...props} />))`
+const Item = styled((props) => <TreeItem {...props} />)`
   && {
-    [class*='MuiTypography-body1']{
+    // todo: we should step away from modifying elements via class names
+    [class*="MuiTypography-body1"] {
       font-family: Inter, sans-serif;
       font-size: 16px;
       font-weight: 500 !important;
@@ -28,25 +29,31 @@ const Item = styled(props => (<TreeItem {...props} />))`
       font-stretch: normal;
       line-height: 1.5;
       letter-spacing: 0.15px;
-      color: rgba(1,1,10,0.6);
+      color: rgba(1, 1, 10, 0.6);
     }
-    [class*='MuiTreeItem-content']{
+    // todo: we should step away from modifying elements via class names
+    [class*="MuiTreeItem-content"] {
       margin-bottom: 42px;
     }
-    [class*='MuiTreeItem-root']{
+    // todo: we should step away from modifying elements via class names
+    [class*="MuiTreeItem-root"] {
       margin-bottom: 42px;
     }
-    [class*='MuiCollapse-wrapperInner']{
+    // todo: we should step away from modifying elements via class names
+    [class*="MuiCollapse-wrapperInner"] {
       margin-bottom: 42px;
     }
-    [class*='MuiSvgIcon-root']{
+    // todo: we should step away from modifying elements via class names
+    [class*="MuiSvgIcon-root"] {
       fill: rgba(1, 1, 10, 0.6);
     }
-    [class*='MuiTreeItem-label']:hover{
+    // todo: we should step away from modifying elements via class names
+    [class*="MuiTreeItem-label"]:hover {
       color: #25a898;
       background-color: #f0f3f7 !important;
     }
-    [class*='MuiTreeItem-label']{
+    // todo: we should step away from modifying elements via class names
+    [class*="MuiTreeItem-label"] {
       background-color: unset !important;
     }
   }
@@ -55,14 +62,18 @@ const Item = styled(props => (<TreeItem {...props} />))`
 export function TreeItemLink(props: Props) {
   return (
     <>
-      {props.item.item ?
-          <Item nodeId={props.item.name} label={props.item.name}>
-            <Link to={`${props.postmanId}#${props.item.name}`}/>
-            {props.children}
-          </Item>
-        :
-        <SubLink3 requestMethod={props.item.request.method} text={props.item.name} to={`${props.postmanId}#${props.item.name}`}/>
-      }
+      {props.item.item ? (
+        <Item nodeId={props.item.name} label={props.item.name}>
+          <Link to={`${props.postmanId}#${props.item.name}`} />
+          {props.children}
+        </Item>
+      ) : (
+        <SubLink3
+          requestMethod={props.item.request.method}
+          text={props.item.name}
+          to={`${props.postmanId}#${props.item.name}`}
+        />
+      )}
     </>
   );
 }
--- src/app/modules/Documentation/common/DocSideBar/index.tsx
+++ src/app/modules/Documentation/common/DocSideBar/index.tsx
@@ -1,24 +1,24 @@
 // @ts-nocheck
 //cc:api documentation module#; navigation - side bar
-import React from 'react';
-import Box from '@material-ui/core/Box';
-import SearchIcon from '@material-ui/icons/Search';
-import { useStoreState } from 'app/modules/Documentation/state/store';
-import { Root } from 'app/modules/Documentation/state/RootModel';
-import { getResults } from 'app/modules/Documentation/common/DocSideBar/utils';
-import { CustomLink } from 'app/modules/Documentation/common/DocSideBar/common/ui/CustomLink';
-import { DocsideBarContainer } from 'app/modules/Documentation/common/DocSideBar/common/ui/DocsideBarContainer';
-import { DocsideBarNavList } from 'app/modules/Documentation/common/DocSideBar/common/ui/DocsideBarNavList';
-import { ApiDocSearchBoxContainer } from 'app/modules/Documentation/common/DocSideBar/common/ui/ApiDocSearchBoxContainer';
-import { ApiDocSearchInput } from 'app/modules/Documentation/common/DocSideBar/common/ui/ApiDocSearchInput';
-import { TreeView } from '@material-ui/lab';
-import { Folder, FolderOpen } from '@material-ui/icons';
-import { TreeItemLink } from './common/TreeItem';
+import React from "react";
+import Box from "@material-ui/core/Box";
+import SearchIcon from "@material-ui/icons/Search";
+import { useStoreState } from "app/modules/Documentation/state/store";
+import { Root } from "app/modules/Documentation/state/RootModel";
+import { getResults } from "app/modules/Documentation/common/DocSideBar/utils";
+import { CustomLink } from "app/modules/Documentation/common/DocSideBar/common/ui/CustomLink";
+import { DocsideBarContainer } from "app/modules/Documentation/common/DocSideBar/common/ui/DocsideBarContainer";
+import { DocsideBarNavList } from "app/modules/Documentation/common/DocSideBar/common/ui/DocsideBarNavList";
+import { ApiDocSearchBoxContainer } from "app/modules/Documentation/common/DocSideBar/common/ui/ApiDocSearchBoxContainer";
+import { ApiDocSearchInput } from "app/modules/Documentation/common/DocSideBar/common/ui/ApiDocSearchInput";
+import { TreeView } from "@material-ui/lab";
+import { Folder, FolderOpen } from "@material-ui/icons";
+import { TreeItemLink } from "./common/TreeItem";
 
 export function DocsideBar() {
-  const data: Root = useStoreState(state => state.data && state.data);
+  const data: Root = useStoreState((state) => state.data && state.data);
   const categories = data && data.collection;
-  const [searchVal, setSearchVal] = React.useState('');
+  const [searchVal, setSearchVal] = React.useState("");
 
   return (
     <DocsideBarContainer>
@@ -26,18 +26,18 @@ export function DocsideBar() {
       <ApiDocSearchBoxContainer>
         <ApiDocSearchInput
           placeholder="Search"
-          inputProps={{ 'aria-label': 'search' }}
-          onChange={e => setSearchVal(e.target.value)}
+          inputProps={{ "aria-label": "search" }}
+          onChange={(e) => setSearchVal(e.target.value)}
         />
         <SearchIcon
           css={`
-            color: #A1AEBD;
+            color: #a1aebd;
           `}
         />
       </ApiDocSearchBoxContainer>
       <Box height="62px" />
       {/* 379px is every pixel above this component*/}
-      <div style={{ overflowY: 'auto', height: 'calc(100vh - 379px)' }}>
+      <div style={{ overflowY: "auto", height: "calc(100vh - 379px)" }}>
         <DocsideBarNavList>
           <CustomLink key="Introduction" to="/documentation/introduction">
             Introduction
@@ -63,22 +63,36 @@ export function DocsideBar() {
             defaultCollapseIcon={<FolderOpen />}
             defaultExpandIcon={<Folder />}
           >
-            {categories && getResults(categories.item, searchVal).map(item => (
+            {categories &&
+              getResults(categories.item, searchVal).map((item) => (
                 <TreeItemLink
                   item={item}
                   postmanId={item._postman_id}
+                  key={item._postman_id}
                 >
-                  {item.item && item.item.map(subItem1 => (
-                    <TreeItemLink item={subItem1} postmanId={item._postman_id}>
-                      {subItem1.item && subItem1.item.map(subItem2 => (
-                        <TreeItemLink item={subItem2} postmanId={item._postman_id}>
-                          {subItem2.item && subItem2.item.map(subItem3 => (
-                            <TreeItemLink item={subItem3} postmanId={item._postman_id}/>
+                  {item.item &&
+                    item.item.map((subItem1) => (
+                      <TreeItemLink
+                        item={subItem1}
+                        postmanId={item._postman_id}
+                      >
+                        {subItem1.item &&
+                          subItem1.item.map((subItem2) => (
+                            <TreeItemLink
+                              item={subItem2}
+                              postmanId={item._postman_id}
+                            >
+                              {subItem2.item &&
+                                subItem2.item.map((subItem3) => (
+                                  <TreeItemLink
+                                    item={subItem3}
+                                    postmanId={item._postman_id}
+                                  />
+                                ))}
+                            </TreeItemLink>
                           ))}
-                        </TreeItemLink>
-                      ))}
-                    </TreeItemLink>
-                  ))}
+                      </TreeItemLink>
+                    ))}
                 </TreeItemLink>
               ))}
           </TreeView>
