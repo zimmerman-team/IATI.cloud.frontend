772337b1593919f8edd6141efa6669e8568f2e7f
Jim Fung <jim@zimmermanzimmerman.nl>
2020-06-16 14:54:17 +0200

    test: updated texts

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -5,15 +5,21 @@ import autoRecord from "cypress-autorecord";
 
 const homeText: string[] = [
   "IATI Datastore",
+  /* ------------------- */
   "The IATI Datastore provides data on development and humanitarian spending and projects that address poverty and crises across the world.",
+  /* ------------------- */
   "Find data published by organisations from governments, development finance institutions and UN agencies to NGOs, foundations and the private sector. They have published data according to the IATI Standard, our set of rules and guidance for sharing useful, open data.",
   "Access data through using the Datastore’s API or Query Builder.",
+  /* ------------------- */
   "About Datastore",
-  "The Datastore uses OIPA, an open-source tool created by Zimmerman & Zimmerman. OIPA extracts and stores raw IATI XML files from the IATI Registry and makes it available as API endpoints to build data driven information solutions.",
-  "Query Builder",
+  /* ------------------- */
+  "The Datastore makes use of iati.cloud, an open-source native IATI data service developed by Zimmerman. IATI.cloud extracts and stores raw IATI XML files from the IATI Registry and makes it available as API endpoints to build data driven information solutions.",
+  /* ------------------- */
   "Use the Query Builder to easily search IATI data based on selected filters and have it returned in spreadsheet (CSV or XLS), JSON and XML formats.",
+  /* ------------------- */
   "API Documentation",
-  "For guidance on how to form API queries and the available endpoints and filters please see the OIPA’s technical documentation.",
+  /* ------------------- */
+  "For guidance on how to form API queries and the available API endpoints and filters please see the IATI.cloud technical documentation.",
 ];
 
 describe("Home", function () {
@@ -25,10 +31,12 @@ describe("Home", function () {
   });
 
   it("should show correct text", function () {
+    cy.viewport(1440, 821);
     homeText.map((text) => cy.findAllByText(text).should("exist"));
   });
 
   it("should show navigation and footer", function () {
+    cy.viewport(1440, 821);
     cy.findByTestId("AppBar").should("exist");
     cy.findByTestId("Footer").should("exist");
   });
