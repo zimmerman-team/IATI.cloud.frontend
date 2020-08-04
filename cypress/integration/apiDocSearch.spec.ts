084ac079acf38d58d8319c6e97db0e6c8b1ce46a
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-08-04 15:49:19 +0200

    test: added visual regression to all tests

--- cypress/integration/apiDocSearch.spec.ts
+++ cypress/integration/apiDocSearch.spec.ts
@@ -1,19 +1,23 @@
 // @ts-nocheck
 /// <reference types="cypress" />
 
-describe("API Documentation - search", function () {
+describe('API Documentation - search', function () {
   // autoRecord();
 
-  it("should load the page", function () {
-    cy.visit("/documentation");
+  it('should load the page', function () {
+    cy.visit('/documentation');
   });
 
   it('should be able to type in "Activity List" and only get results with "Activity List" in it', function () {
-    cy.get("[class*= ApiDocSearchBoxContainer]").should("exist");
-    cy.get(".MuiInputBase-input").type("Activity List");
-    cy.get("[class*= MuiTreeView-root]").should(
-      "contain.text",
-      "Activity List"
+    cy.get('[class*= ApiDocSearchBoxContainer]').should('exist');
+    cy.get('.MuiInputBase-input').type('Activity List');
+    cy.get('[class*= MuiTreeView-root]').should(
+      'contain.text',
+      'Activity List'
     );
+  });
+
+  it('make screenshot', () => {
+    cy.get('body').happoScreenshot({ component: 'API docs page' });
   });
 });
