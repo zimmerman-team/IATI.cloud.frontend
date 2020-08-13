47aba170a141e29dc524020f5c4e51a190adb9a8
Juan Albergen <juan@zimmermanzimmerman.nl>
2020-08-13 12:58:59 +0200

    feat: test implementation of happo-cypress flow
    [2396]

--- .happo.js
+++ .happo.js
@@ -1,25 +1,25 @@
 const { RemoteBrowserTarget } = require('happo.io');
 
 // const domain = "https://store.staging.iati.cloud/";
-const domain = 'http://localhost:3000/';
+// const domain = 'http://localhost:3000/';
 
 module.exports = {
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
-  prerender: false,
   project: 'iati-cloud-front-end',
   targets: {
     'chrome-desktop': new RemoteBrowserTarget('chrome', {
       viewport: '1440x1024',
     }),
   },
+  // prerender: false,
 
   // githubApiUrl: "https://api.github.com",
 
-  pages: [
-    { url: domain, title: 'landing' },
-    { url: domain + 'about', title: 'about' },
-    { url: domain + 'querybuilder', title: 'querybuilder' },
-    { url: domain + 'documentation/introduction', title: 'documentation' },
-  ],
+  // pages: [
+  //   { url: domain, title: 'landing' },
+  //   { url: domain + 'about', title: 'about' },
+  //   { url: domain + 'querybuilder', title: 'querybuilder' },
+  //   { url: domain + 'documentation/introduction', title: 'documentation' },
+  // ],
 };
